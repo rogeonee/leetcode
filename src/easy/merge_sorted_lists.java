ed9f631d1a73f81036444506171532375b3c0172
Egor <egormaxb@gmail.com>
2023-01-05 13:35:09 -0800

    Added recursion solution

--- src/easy/merge_sorted_lists.java
+++ src/easy/merge_sorted_lists.java
@@ -104,6 +104,19 @@ public class merge_sorted_lists {
 
         return head;
     }
+
+    public ListNode recursion(ListNode l1, ListNode l2) {
+		if(l1 == null) return l2;
+		if(l2 == null) return l1;
+
+		if(l1.val < l2.val) {
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		} else {
+			l2.next = mergeTwoLists(l1, l2.next);
+			return l2;
+		}
+    }
 }
 
 class ListNode {
