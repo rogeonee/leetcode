de2068c3056b721a892e0bced5dc2bbdb455e13d
Egor <egormaxb@gmail.com>
2023-01-08 16:30:38 -0800

    Not my solution, no idea what's going on there

---
+++ src/easy/flood_fill.java
@@ -0,0 +1,27 @@
+package easy;
+
+public class flood_fill {
+    public static void main(String[] args) {
+
+    }
+
+    // No idea what's going on
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int oldColor = image[sr][sc];
+        if(oldColor == color) 
+            return image;
+        floodFillHelper(image, sr, sc, color, image.length, image[0].length, oldColor);
+        return image;
+    }
+
+    private void floodFillHelper(int[][] image, int sr, int sc, int color, int m, int n, int oldColor) {
+        if((sr>=0 && sr<m) && (sc>=0 && sc<n) && (image[sr][sc]==oldColor)) {
+            image[sr][sc]=color;
+            floodFillHelper(image, sr-1, sc, color, m, n, oldColor);
+            floodFillHelper(image, sr+1, sc, color, m, n, oldColor);
+            floodFillHelper(image, sr, sc-1, color, m, n, oldColor);
+            floodFillHelper(image, sr, sc+1, color, m, n, oldColor);
+        }
+    }
+    
+}
